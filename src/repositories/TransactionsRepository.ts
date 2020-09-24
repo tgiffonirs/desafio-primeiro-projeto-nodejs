0a3b8563a6f2cf0230b27f56d7204230b8c246a6
Thiago Giffoni <giffoni@hotmail.com>
2020-09-24 05:41:13 -0300

    feat: Implementation of functionalities to input and list transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    const balanceTotal = this.transactions.reduce((previous, current) => {
+      const previousBalance = previous;
+
+      previousBalance[current.type] += current.value;
+
+      if (current.type === 'income') previousBalance.total += current.value;
+      else previousBalance.total -= current.value;
+
+      return previousBalance;
+    }, balance);
+
+    return balanceTotal;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({
+      title,
+      type,
+      value,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
